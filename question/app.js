834ae702a23b5185ed99a26c7ad25d4a591078e0
janjan519 <207494690+janjan519@users.noreply.github.com>
2025-09-02 14:08:27 +0900

    feat: Q1~5課題完了

---
+++ question/app.js
@@ -0,0 +1,51 @@
+// Q1
+let nickname = "ごっしー";
+let age = 28;
+
+console.log(
+  "私のニックネームは" + nickname + "です。年齢は" + age + "歳です。"
+);
+
+// Q2
+let languages = ["JavaScript", "PHP", "Ruby", "Python", "Go"];
+
+console.log(
+  `私の好きな言語は${languages[0]}です。次は${languages[3]}を勉強してみたいです。`
+);
+
+// Q3
+let user = {
+  name: "John",
+  age: 26,
+  bloodType: "A",
+  favorite: "card",
+};
+
+console.log(user.age);
+
+// Q4
+let playerList = [
+  {
+    name: "John",
+    age: 26,
+    favorites: ["Card Game", "Basket Ball", "Programming"],
+  },
+  {
+    name: "Bob",
+    age: 33,
+    favorites: ["Tinder", "The Legend of Zelda"],
+  },
+  {
+    name: "Michael",
+    age: 22,
+    favorites: ["Football", "Smash Bros."],
+  },
+];
+
+console.log(playerList[1].favorites[1]);
+
+// Q5 Q4 で定義した変数 playerList を使用し、John, Bob, Michael の年齢の平均値を計算した結果をコンソールに出力してください。
+let ageList = playerList.map((player) => player.age);
+let ageAvg = ageList.reduce((acc, cur) => acc + cur, 0) / ageList.length;
+
+console.log(ageAvg);
